6b3dc32bdfc5aa2a58bb6e54a18f6fe89276f087
Denis <i@denis.by>
2016-03-22 22:53:05 +0300

    Init. Typing carousel as directive for angular2.

---
+++ ng2-typing-carousel.ts
@@ -0,0 +1,3 @@
+///<reference path="./node_modules/angular2/typings/browser.d.ts"/>
+import TypingCarouselDirective from './src/typing-carousel.directive'
+export default TypingCarouselDirective;
---
+++ src/typing-carousel.directive.ts
@@ -0,0 +1,55 @@
+import {Directive, ElementRef, Input} from 'angular2/core';
+
+@Directive({
+    selector: '[typingCarousel]',
+    inputs: ['typingCarousel', 'period']
+})
+export class TypingCarouselDirective {
+
+    element:ElementRef;
+    loopNum:number;
+    period:number;
+    isDeleting:boolean;
+    typingCarousel:Array<string>;
+
+    constructor(el:ElementRef) {
+        this.element = el;
+        this.loopNum = 0;
+        this.period = 1000;
+        this.isDeleting = false;
+        this.typingCarousel = [];
+    }
+
+    ngOnInit() {
+        this.tick('', this.typingCarousel, this.period);
+    }
+
+    tick(value:string, toRotate:Array<string>, period:number) {
+        let i = this.loopNum % toRotate.length,
+            fullTxt = toRotate[i],
+            delta = 300 - Math.random() * 100;
+
+        value = this.isDeleting ?
+            fullTxt.substring(0, value.length - 1) :
+            fullTxt.substring(0, value.length + 1);
+
+        if (this.isDeleting) {
+            delta /= 2;
+        }
+
+        if (!this.isDeleting && value === fullTxt) {
+            delta = period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && value === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        this.element.nativeElement.innerHTML = value;
+
+        setTimeout(() => this.tick(value, toRotate, period), delta);
+    }
+}
+
+export default TypingCarouselDirective;
